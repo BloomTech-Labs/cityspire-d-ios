f81549685e9ab5941c8fc563cbfa9a81c9e80839
Kelson Hartle <kelson99hartle@icloud.com>
2021-01-26 20:34:34 -0700

    Basic Favorites Collection View now working

--- labs-ios-starter/FavoritesCollectionViewController.swift
+++ labs-ios-starter/FavoritesCollectionViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class FavoritesCollectionViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let cities = ["New York", "Rio", "Orlando", "Miami", "San Diego", "Houston", "Kansas City"]
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.backgroundColor = .lightGray
+        collectionView.backgroundColor = .lightGray
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -25,6 +31,38 @@ class FavoritesCollectionViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout, UICollectionViewDataSource
+extension FavoritesCollectionViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! FavoriteCollectionViewCell
+        
+        //cell.layer.borderWidth = 2
+        //cell.layer.borderColor = UIColor.blue.cgColor
+        cell.layer.cornerRadius = 10
+        let currentObject = cities[indexPath.row]
+        cell.cityNameLabel.text = currentObject
+//        cell.backgroundView = cell.backgroundImageView
+        cell.backgroundView = UIImageView.init(image: UIImage(named: currentObject))
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width) - 30, height: view.bounds.height / 2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
+    
 }
