5b8f0d800838c88f54e4e2740a42e9e78dabeb59
Kelson Hartle <kelson99hartle@icloud.com>
2021-01-26 20:33:47 -0700

    Created Basic Favorites Collection View

---
+++ labs-ios-starter/FavoriteCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteCollectionViewCell.swift
+//  labs-ios-starter
+//
+//  Created by Kelson Hartle on 1/26/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ labs-ios-starter/FavoritesCollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesCollectionViewController.swift
+//  labs-ios-starter
+//
+//  Created by Kelson Hartle on 1/25/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
