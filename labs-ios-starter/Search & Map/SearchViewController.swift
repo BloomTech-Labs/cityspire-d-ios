9f0b0c064b18ceb391ed3cecd27d476b92386ef3
Chad Parker <chad@chadparker.com>
2021-01-27 20:18:25 -0800

    Create SearchViewController

---
+++ labs-ios-starter/Search & Map/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  labs-ios-starter
+//
+//  Created by Chad Parker on 1/27/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
