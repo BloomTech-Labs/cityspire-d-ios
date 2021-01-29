ec9b41ef96b1d1339d9cb6b565901df179a9a25e
Kelson Hartle <kelson99hartle@icloud.com>
2021-01-29 14:31:20 -0700

    Created basic model Structure
    Created model data for Location data and for air quality.

---
+++ labs-ios-starter/LocationData.swift
@@ -0,0 +1,45 @@
+//
+//  LocationData.swift
+//  labs-ios-starter
+//
+//  Created by Kelson Hartle on 1/29/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+//7cc90139-bfec-4859-8f55-fca7bf666056
+// get the aqi air quality index
+
+struct LocationData : Codable {
+    let cityName: String
+    let latitude: Double
+    let longitude: Double
+    let population: Int
+    let rank: Int
+    let stateName: String
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case cityName = "city"
+        case latitude
+        case longitude
+        case population
+        case rank
+        case stateName = "state"
+    }
+}
+
+struct AirQualityData : Codable {
+//    /http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key={{YOUR_API_KEY}}
+
+    let pollution: Pollution
+}
+
+struct Pollution : Codable {
+    let airQualityIndex: String
+    
+    enum CodingKeys: String, CodingKey {
+        case airQualityIndex = "aqius"
+    }
+}
