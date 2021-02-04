bf565b5028c47bbeceda8855a6e9f99e54e64792
Kelson Hartle <kelson99hartle@icloud.com>
2021-02-03 20:48:51 -0700

    Implemented container view for detail view for map and favorites
    Simple implementation of detail view for our favorites and map view controller.

---
+++ labs-ios-starter/MapContainerViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapContainerViewController.swift
+//  labs-ios-starter
+//
+//  Created by Kelson Hartle on 2/3/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapContainerViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mapView.isUserInteractionEnabled = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
