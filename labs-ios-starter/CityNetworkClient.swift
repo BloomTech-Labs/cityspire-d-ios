a90edd6c61d3aef1445cc8c6eb619ab15da39ee9
Kelson Hartle <kelson99hartle@icloud.com>
2021-02-10 22:08:32 -0700

    Created Functions to fetch each score for a city

--- labs-ios-starter/CityNetworkClient.swift
+++ labs-ios-starter/CityNetworkClient.swift
@@ -57,9 +57,9 @@ class CityNetworkClient {
 //      "score": 4,
 //      "avg_rent": 2100
 //    }
-    func fetchAverageRent(forCity city: String, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+    func fetchAverageRent(forCity city: String, completion: @escaping (Result<Rent, NetworkError>) -> Void) {
         
-        let averageRentForCityURL = baseDSURL.appendingPathComponent("city_scr/\(city)")
+        let averageRentForCityURL = baseDSURL.appendingPathComponent("rent_scr/\(city)")
         
         URLSession.shared.dataTask(with: averageRentForCityURL) { (data, _, error) in
             if let error = error {
@@ -72,14 +72,12 @@ class CityNetworkClient {
             }
             
             do {
-                let decodedCities = try self.jsonDecoder.decode([City].self, from: data)
-                self.cities = decodedCities
+                let rent = try self.jsonDecoder.decode(Rent.self, from: data)
+                completion(.success(rent))
             } catch {
                 print("Error unable to decode the data")
                 completion(.failure(.noDecode))
             }
-            
-            completion(.success(true))
         }.resume()
     }
     
@@ -91,22 +89,119 @@ class CityNetworkClient {
 //      "score": 4,
 //      "raw_score": 75
 //    }
+    func fetchWalkScore(forCity city: String, completion: @escaping (Result<WalkScore, NetworkError>) -> Void) {
+        
+        let walkScoreURL = baseDSURL.appendingPathComponent("walk_scr/\(city)")
+        
+        URLSession.shared.dataTask(with: walkScoreURL) { (data, _, error) in
+            if let error = error {
+                print("Error \(error)")
+            }
+            
+            guard let data = data else {
+                print("No Data Returned from API")
+                return
+            }
+            
+            do {
+                let walkScore = try self.jsonDecoder.decode(WalkScore.self, from: data)
+                completion(.success(walkScore))
+            } catch {
+                print("Error unable to decode the data")
+                completion(.failure(.noDecode))
+            }
+        }.resume()
+    }
+    
+    
 //    // crime score
 //    {
 //      "msg": "crime score",
 //      "score": 4,
 //    }
+    
+    func fetchCrimeScore(forCity city: String, completion: @escaping (Result<CrimeScore, NetworkError>) -> Void) {
+        
+        let crimeScoreCityURL = baseDSURL.appendingPathComponent("crime_scr/\(city)")
+        
+        URLSession.shared.dataTask(with: crimeScoreCityURL) { (data, _, error) in
+            if let error = error {
+                print("Error \(error)")
+            }
+            
+            guard let data = data else {
+                print("No Data Returned from API")
+                return
+            }
+            
+            do {
+                let crimeScore = try self.jsonDecoder.decode(CrimeScore.self, from: data)
+                completion(.success(crimeScore))
+            } catch {
+                print("Error unable to decode the data")
+                completion(.failure(.noDecode))
+            }
+        }.resume()
+    }
 //
 //    // airquality score
 //    {
 //      "msg": "air quality score",
 //      "score": 5,
 //    }
+    
+    func fetchAirQuality(forCity city: String, completion: @escaping (Result<AirQuality, NetworkError>) -> Void) {
+        
+        let airQualityCityURL = baseDSURL.appendingPathComponent("air_qual_scr/\(city)")
+        
+        URLSession.shared.dataTask(with: airQualityCityURL) { (data, _, error) in
+            if let error = error {
+                print("Error \(error)")
+            }
+            
+            guard let data = data else {
+                print("No Data Returned from API")
+                return
+            }
+            
+            do {
+                let airQuality = try self.jsonDecoder.decode(AirQuality.self, from: data)
+                completion(.success(airQuality))
+            } catch {
+                print("Error unable to decode the data")
+                completion(.failure(.noDecode))
+            }
+        }.resume()
+    }
 //
 //    // overall score
 //    {
 //      "msg": "overall quality of life score",
 //      "score": 4,
 //    }
+    
+    func fetchLifeScore(forCity city: String, completion: @escaping (Result<LifeScore, NetworkError>) -> Void) {
+        
+        let averageScoreCityURL = baseDSURL.appendingPathComponent("city_scr/\(city)")
+        
+        URLSession.shared.dataTask(with: averageScoreCityURL) { (data, _, error) in
+            if let error = error {
+                print("Error \(error)")
+            }
+            
+            guard let data = data else {
+                print("No Data Returned from API")
+                return
+            }
+            
+            do {
+                let lifeScore = try self.jsonDecoder.decode(LifeScore.self, from: data)
+                completion(.success(lifeScore))
+            } catch {
+                print("Error unable to decode the data")
+                completion(.failure(.noDecode))
+            }
+        }.resume()
+    }
     
 }
