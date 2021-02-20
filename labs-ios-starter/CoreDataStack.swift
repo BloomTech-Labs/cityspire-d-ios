0e63262a1040003bbb30b61f7c0d081fc4f8679e
Kelson Hartle <kelson99hartle@icloud.com>
2021-02-20 12:41:07 -0700

    implemented core data stack

--- labs-ios-starter/CoreDataStack.swift
+++ labs-ios-starter/CoreDataStack.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "labs-ios-starter")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func saveToPersistentStore() {
+        do {
+            try mainContext.save()
+        } catch {
+            NSLog("Error saving context: \(error)")
+            mainContext.reset()
+        }
+    }
+    
+}
